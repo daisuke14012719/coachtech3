1f266977c12b679c57a04e25ab4c501dad64b715
daisuke14012719 <79070611+daisuke14012719@users.noreply.github.com>
2021-04-08 12:15:26 +0900

    Add files via upload

---
+++ 0408_確認テスト/router-pj/src/views/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+    <ul>
+      <li>
+        <router-link :to="{ name: 'Member' , params: { id: 1} }">
+          1へ進む
+        </router-link>
+      </li>
+      <li>
+        <router-link :to="{ name: 'Member' , params: { id: 2} }">
+          2へ進む
+        </router-link>
+      </li>
+      <li>
+        <router-link :to="{ name: 'Member' , params: { id: 3} }">
+          3へ進む
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+ul {
+  padding:0px;
+}
+li{
+  list-style:none;
+}
+</style>
+
+<script>
+export default{
+  methods:{
+    transition(){
+      this.$router.push({name: "Member" , params: { id: 1} });
+      this.$router.push({name: "Member" , params: { id: 2} });
+      this.$router.push({name: "Member" , params: { id: 3} });
+    } 
+  }
+};
+
+</script>
